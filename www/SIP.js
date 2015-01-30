81c975ceda58f4b0a93c5285a887600a74531143
Fco. Javier Martín Carrasco <javray@gmail.com>
2015-01-30 16:12:50 +0100

    call, callEnd

--- www/SIP.js
+++ www/SIP.js
@@ -7,16 +7,12 @@ SIP.prototype.connect = function(arg0, arg1, arg2, success, error) {
     exec(success, error, "SIP", "connect", [arg0, arg1, arg2]);
 };
 
-SIP.prototype.disconnect = function(success, error) {
-    exec(success, error, "SIP", "disconnect", []);
+SIP.prototype.call = function(arg0, success, error) {
+    exec(success, error, "SIP", "call", [arg0]);
 };
 
-SIP.prototype.login = function(arg0, arg1, success, error) {
-    exec(success, error, "SIP", "login", [arg0, arg1]);
-};
-
-SIP.prototype.command = function(arg0, success, error) {
-    exec(success, error, "SIP", "command", [arg0]);
+SIP.prototype.callend = function(success, error) {
+    exec(success, error, "SIP", "callend", []);
 };
 
 module.exports = new SIP();

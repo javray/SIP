547b510ad27e6b1ab9e5aa7f2f3bc692d3d7beae
Fco. Javier Martín Carrasco <javray@gmail.com>
2015-02-02 15:26:47 +0100

    RingingBack

--- src/android/SIP.java
+++ src/android/SIP.java
@@ -134,7 +134,7 @@ public class SIP extends CordovaPlugin {
         }
         setInCallMode();
         //mRingbackTone.startTone(ToneGenerator.TONE_CDMA_LOW_PBX_L);
-        mRingbackTone.startTone(ToneGenerator.TONE_CDMA_NETWORK_USA_RINGBACK);
+        mRingbackTone.startTone(ToneGenerator.TONE_SUP_CALL_WAITING);
 
     }
 

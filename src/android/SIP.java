ae3892376fd284c10999633dc5c52a2e6eb3bd99
Fco. Javier Martín Carrasco <javray@gmail.com>
2015-02-02 12:32:55 +0100

    RingingBack

--- src/android/SIP.java
+++ src/android/SIP.java
@@ -134,6 +134,7 @@ public class SIP extends CordovaPlugin {
         }
         Log.d("SAREMOVIL", "1. RingbackTone: " + mRingbackTone);
         //setInCallMode();
+        setSpeakerMode();
         Log.d("SAREMOVIL", "2. RingbackTone: " + mRingbackTone);
         mRingbackTone.startTone(ToneGenerator.TONE_CDMA_LOW_PBX_L);
         Log.d("SAREMOVIL", "3. RingbackTone: " + mRingbackTone);

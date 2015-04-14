c99ef72b0eb1f0f1784744a433aefe8a2a005dad
Fco. Javier Martín Carrasco <javi@sarenet.es>
2015-04-14 14:21:51 +0200

    doubango

--- src/android/SIPService.java
+++ src/android/SIPService.java
@@ -13,8 +13,6 @@ public class SIPService extends NgnApplication {
 
   public SIPService() {
 
-    Log.d("SIPService", NgnApplication.getContext().getPackageName());
-
     mEngine = NgnEngine.getInstance();
   }
 

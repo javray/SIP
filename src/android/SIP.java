532fd9e6daf0d85c0d57f5ebb0c67dad115168ee
Fco. Javier Martín Carrasco <javi@sarenet.es>
2015-04-14 10:22:54 +0200

    doubango

--- src/android/SIP.java
+++ src/android/SIP.java
@@ -25,10 +25,6 @@ import android.net.sip.SipRegistrationListener;
 
 import android.util.Log;
 
-import org.doubango.ngn.NgnEngine;
-import org.doubango.ngn.services.INgnConfigurationService;
-import org.doubango.ngn.utils.NgnConfigurationEntry;
-
 public class SIP extends CordovaPlugin {
 
   private Context mContext;
@@ -42,10 +38,8 @@ public class SIP extends CordovaPlugin {
   private SipAudioCall call = null;
 
   private CordovaWebView appView = null;
-
-  private NgnEngine mEngine = null;
-  private NgnEngine mEngineClass = null;
-
+  
+  private SIPService mSIPService = null;
 
   public SIP() {
   }
@@ -56,31 +50,18 @@ public class SIP extends CordovaPlugin {
 
     appView = webView;
 
-    mEngineClass = new NgnEngine(cordova);
+    mSIPService = new SIPService();
   }
 
   private void connectSip(String user, String pass, String domain, CallbackContext callbackContext) {
 
     mContext = cordova.getActivity();
 
-    mEngine = mEngineClass.getInstance();
-
     mSipManager = SipManager.newInstance(mContext);
 
     if (mSipManager.isVoipSupported(mContext)) {
 
-      INgnConfigurationService mConfigurationService = mEngine.getConfigurationService();
-
-      mConfigurationService.putString(NgnConfigurationEntry.IDENTITY_IMPI, user);
-      mConfigurationService.putString(NgnConfigurationEntry.IDENTITY_IMPU, String.format("sip:%s@%s", user, domain));
-      mConfigurationService.putString(NgnConfigurationEntry.IDENTITY_PASSWORD, pass);
-      mConfigurationService.putString(NgnConfigurationEntry.NETWORK_PCSCF_HOST, domain);
-      mConfigurationService.putInt(NgnConfigurationEntry.NETWORK_PCSCF_PORT, NgnConfigurationEntry.DEFAULT_NETWORK_PCSCF_PORT);
-      mConfigurationService.putString(NgnConfigurationEntry.NETWORK_REALM, domain);
-      mConfigurationService.putBoolean(NgnConfigurationEntry.NETWORK_USE_3G, true);
-      mConfigurationService.putInt(NgnConfigurationEntry.NETWORK_REGISTRATION_TIMEOUT, 3600);
-
-      mConfigurationService.commit();
+      mSIPService.connectSip(user, pass, domain);
 
     }
     else {
---
+++ src/android/SIPService.java
@@ -0,0 +1,31 @@
+package com.javray.cordova.plugin;
+
+import org.doubango.ngn.NgnEngine;
+import org.doubango.ngn.services.INgnConfigurationService;
+import org.doubango.ngn.utils.NgnConfigurationEntry;
+
+public class SIPService extends NgnApplication {
+
+  private NgnEngine mEngine = null;
+
+  public SIPService() {
+  }
+
+  public void connectSip(String user, String pass, String domain) {
+
+    mEngine = NgnEngine.getInstance();
+
+    INgnConfigurationService mConfigurationService = mEngine.getConfigurationService();
+
+    mConfigurationService.putString(NgnConfigurationEntry.IDENTITY_IMPI, user);
+    mConfigurationService.putString(NgnConfigurationEntry.IDENTITY_IMPU, String.format("sip:%s@%s", user, domain));
+    mConfigurationService.putString(NgnConfigurationEntry.IDENTITY_PASSWORD, pass);
+    mConfigurationService.putString(NgnConfigurationEntry.NETWORK_PCSCF_HOST, domain);
+    mConfigurationService.putInt(NgnConfigurationEntry.NETWORK_PCSCF_PORT, NgnConfigurationEntry.DEFAULT_NETWORK_PCSCF_PORT);
+    mConfigurationService.putString(NgnConfigurationEntry.NETWORK_REALM, domain);
+    mConfigurationService.putBoolean(NgnConfigurationEntry.NETWORK_USE_3G, true);
+    mConfigurationService.putInt(NgnConfigurationEntry.NETWORK_REGISTRATION_TIMEOUT, 3600);
+
+    mConfigurationService.commit();
+  }
+}

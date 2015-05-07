e42a522eafa3ef1460832bd94bf9ea80042e3d0a
Fco. Javier Martín Carrasco <javi@sarenet.es>
2015-05-07 09:07:25 +0200

    listen SIP calls

--- src/android/SIPReceiver.java
+++ src/android/SIPReceiver.java
@@ -4,11 +4,25 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.BroadcastReceiver;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo.State;
+
 import android.util.Log;
 
 public class SIPReceiver extends BroadcastReceiver {
   @Override
   public void onReceive(Context context, Intent intent) {
+
       Log.d("SIP", "Llamada recibida");
+
+      ConnectivityManager conMan = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+      //State mobile = conMan.getNetworkInfo(0).getState();
+
+      State wifi = conMan.getNetworkInfo(1).getState();
+
+      if (wifi == State.CONNECTED || wifi == State.CONNECTING) {
+        Log.d("SIP", "WIFI");
+      }
   }
 }

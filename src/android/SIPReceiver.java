c087d8b420e10687c4f8b7796f6e90d1d1e2d54b
Fco. Javier Martín Carrasco <javi@sarenet.es>
2015-05-07 08:42:46 +0200

    listen SIP calls

--- src/android/SIPReceiver.java
+++ src/android/SIPReceiver.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.BroadcastReceiver;
 
+import android.util.Log;
+
 public class SIPReceiver extends BroadcastReceiver {
   @Override
   public void onReceive(Context context, Intent intent) {
